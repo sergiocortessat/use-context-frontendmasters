5bfd75bce8cf0fe08f15815b2c94715ee3407976
sbouden16 <sergiocortessat@gmail.com>
2021-11-17 17:58:11 +0100

    Add changes

--- src/context.tsx
+++ src/context.tsx
@@ -1,6 +1,6 @@
 import * as React from "react";
 import { AdjustmentAction, reducer } from "./reducer";
-
+// ggg
 import { RGBColorType } from "./types";
 
 interface RGBContextType extends RGBColorType {
